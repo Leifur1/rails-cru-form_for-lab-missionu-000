17fd23b893f343c1e77dee9684df4b15e5fdc49d
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-15 23:01:49 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,19 +1,35 @@
-class GenreController < ApplicationController
-  def index
-  end
+class GenresController < ApplicationController
+	def index
+		@genres = Genre.all
+	end
 
-  def show
-  end
+	def show
+		@genre = Genre.find(params[:id])
+	end
 
-  def new
-  end
+	def new
+		@genre = Genre.new
+	end
 
-  def create
-  end
+	def create
+	  @genre = Genre.new(genre_params)
+	  @genre.save
+	  redirect_to genre_path(@genre)
+	end
 
-  def edit
-  end
+	def edit
+		@genre = Genre.find(params[:id])
+	end
 
-  def update
-  end
+	def update
+	  @genre = Genre.find(params[:id])
+	  @genre.update(genre_params)
+	  redirect_to genre_path(@genre)
+	end
+
+	private
+
+		def genre_params
+			params.require(:genre).permit(:name)
+		end
 end
